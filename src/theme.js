0be3ec1d148e043b828a060c0abea18eaf595d35
redhair <thomasbonanni@afito.com>
2019-08-22 10:25:15 -0400

    updated peer/dev dependencies

--- src/theme.js
+++ src/theme.js
@@ -26,7 +26,7 @@ const theme = {
   gridSmWidth: '100%',
   gridMdWidth: '900px',
   gridLgWidth: '1050px',
-  gridXlWidth: '1600px',
+  gridXlWidth: '1520px',
 
   gutterXs: '8px',
   gutterSm: '8px',
