3e2a4fa0934fbd6d913cf8e2b220a86f7d240c01
redhair <thomasbonanni@afito.com>
2019-08-27 19:58:40 -0400

    updated modal default styles

--- src/components/ModalProvider/index.js
+++ src/components/ModalProvider/index.js
@@ -50,13 +50,53 @@ class ModalProvider extends Component {
   }
 }
 
-const Modal = ({ children }) => {
+const Header = styled.div`
+  border-top-right-radius: 8px;
+  border-top-left-radius: 8px;
+  padding: 28px;
+  background: white;
+  border-bottom: 1px solid #dcdcdc;
+  margin: 0;
+`;
+
+const Body = styled.div`
+  padding: 28px;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  border-top: 1px solid #dcdcdc;
+  padding: 0 10px;
+  background: white;
+  border-bottom-right-radius: 8px;
+  border-bottom-left-radius: 8px;
+`;
+
+const ModalWrapper = styled(ResponsiveModal)`
+  background-color: white;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  padding: 0px;
+  max-width: 500px;
+  width: 100%;
+
+  @media screen and (max-width: 500px) {
+    max-width: 100%;
+    width: 100%;
+  }
+`;
+
+const Modal = ({ header, footer, children, ...rest }) => {
   const { visible, hideModal } = useContext(ModalContext);
 
   return (
-    <ResponsiveModal open={visible} onClose={() => hideModal()} center blockScroll={false}>
-      {children}
-    </ResponsiveModal>
+    <ModalWrapper center open={visible} onClose={() => hideModal()} blockScroll={false} {...rest}>
+      <Header>{header}</Header>
+      <Body>{children}</Body>
+      <Footer>{footer}</Footer>
+    </ModalWrapper>
   );
 };
 
