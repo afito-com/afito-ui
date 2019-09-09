f4a767d1bdc4926a645035b3f3c9a76cd9bb59a6
redhair <thomasbonanni@afito.com>
2019-09-09 14:07:12 -0400

    testing ssr bug v2

--- src/components/Range/index.js
+++ src/components/Range/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import InputRange from 'react-input-range';
-import './Range.scss';
+//import './Range.scss';
 
 const Wrapper = styled.div`
   display: flex;
