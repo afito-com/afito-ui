b8b00400798b8ecebbdb98f89cc8d78f92c7b9fe
redhair <thomasbonanni@afito.com>
2019-09-25 12:40:48 -0400

    made area card responsive [unfix]

--- src/components/AreaCard/index.js
+++ src/components/AreaCard/index.js
@@ -29,7 +29,6 @@ const Image = styled.div`
   height: 380px;
   overflow: hidden;
   width: 100%;
-  max-width: 292px;
   border-radius: 8px;
   background-size: cover;
   background-repeat: no-repeat;
