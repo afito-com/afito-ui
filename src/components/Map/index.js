929717686d24472808eb835aa478e67e22d0a5d1
redhair <thomasbonanni@afito.com>
2019-12-19 22:24:01 -0500

    added defaultZoom to map component

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -76,20 +76,30 @@ const MemoFloatingPropertyCard = React.memo(props => {
 });
 
 const Map = withScriptjs(
-  withGoogleMap(({ properties, center, hoveredProperty = {}, onMarkerClick, onPropertyHover, onCenterChanged }) => {
-    return (
-      <GoogleMap defaultZoom={15} defaultCenter={center} onCenterChanged={onCenterChanged}>
-        <Markers
-          properties={properties}
-          onPropertyHover={onPropertyHover}
-          onMarkerClick={onMarkerClick}
-          hoverId={hoveredProperty.property_id}
-        />
+  withGoogleMap(
+    ({
+      properties,
+      center,
+      hoveredProperty = {},
+      defaultZoom = 15,
+      onMarkerClick,
+      onPropertyHover,
+      onCenterChanged
+    }) => {
+      return (
+        <GoogleMap defaultZoom={defaultZoom} defaultCenter={center} onCenterChanged={onCenterChanged}>
+          <Markers
+            properties={properties}
+            onPropertyHover={onPropertyHover}
+            onMarkerClick={onMarkerClick}
+            hoverId={hoveredProperty.property_id}
+          />
 
-        {Object.keys(hoveredProperty).length > 0 && <MemoFloatingPropertyCard hoveredProperty={hoveredProperty} />}
-      </GoogleMap>
-    );
-  })
+          {Object.keys(hoveredProperty).length > 0 && <MemoFloatingPropertyCard hoveredProperty={hoveredProperty} />}
+        </GoogleMap>
+      );
+    }
+  )
 );
 
 Map.propTypes = {
